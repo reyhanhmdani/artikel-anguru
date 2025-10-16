28f6768f3f6b6c7e67389eb775c8142c11583430
reyhanhmdani <rey7dan7@gmail.com>
2025-10-16 13:40:24 +0700

    add ver GSC

--- resources/views/includes/public-head.blade.php
+++ resources/views/includes/public-head.blade.php
@@ -3,6 +3,7 @@
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 <title>@yield('title')</title>
+<meta name="google-site-verification" content="_JWB4jLor78m-AK2vATHgQ7xGBpbYWd_D0oo3ROb4B8" />
 
 <link rel="icon" type="image/png" href="{{ asset('assets/icon/ARDC.png') }}">
 {{-- Menggunakan helper asset() Laravel untuk path CSS Anda --}}
